cb4b574ad6d5f60d2df51aa2fc1fedd7b1aff8b4
Kosma001 <kosmaskawinski001@gmail.com>
2025-09-21 22:50:11 +0200

    Add files via upload

---
+++ Skawiński_zewnetrzny.js
@@ -0,0 +1,5 @@
+document.write("ostatnia modyfikacja strony".fontcolor("green").bold().fontsize(7)+"<br>");
+document.write(document.lastModified +"<br>");
+document.write("Mój numer w dzienniku to 21"+"<br>");
+document.write("Pierwszy wiersz to: 9 Nie Tak 6".strike().fontsize(6)+"<br>");
+document.write("Drugi wiersz to: 9 Nie Tak 6".strike().fontsize(6)+"<br>");
